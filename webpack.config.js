f18607bcb3bdb124a3b82a58ba7dd1b46a43a2ce
milliwonkim <rldnjs9347@gmail.com>
2022-03-23 02:07:05 +0900

    renew react project

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/index.js',
-    output: { path: path.join(__dirname, '/dist'), filename: 'bundle.js' },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // change this
+    publicPath: '/',
+    filename: 'bundle.[hash].js',
+  },
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html'),
+    }),
+  ],
+  devtool: 'inline-source-map',
 };
